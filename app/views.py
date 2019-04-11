d923a1921af7676596f1cb3c5f1c3bbd4cbce079
yuktaanand <yukta.19feb@gmail.com>
2019-04-12 02:51:29 +0530

    Changed resource id

--- app/views.py
+++ app/views.py
@@ -4,12 +4,12 @@ import requests
 from todoist.api import TodoistAPI
 import json
 
-# Create your views here.
+
 def initials(request):
     res = requests.post(
         url='https://app.asana.com/api/1.0/webhooks',
         headers={"Authorization": "Bearer 0/92f79995f7c1b4b0f751b9a07c8e52c2"},
-        data={"resource": "1116664462589700write_workspace_ id", "target": "http://127.0.0.1:8000/payload/"}
+        data={"resource": "1116265931583433", "target": "http://127.0.0.1:8000/payload/"}
     )
 
 
@@ -27,24 +27,23 @@ def handle_payload(request):
                 if type == "task":
                     url = "https://app.asana.com/api/1.0/tasks/" + str(id)
 
-            if not url == None:
-                data = requests.get(
-                    url=url,
-                    headers={"Authorization": "Bearer 0/92f79995f7c1b4b0f751b9a07c8e52c2"}
-                )
+                if not url == None:
+                    data = requests.get(
+                        url=url,
+                        headers={"Authorization": "Bearer 0/92f79995f7c1b4b0f751b9a07c8e52c2"}
+                    )
 
-            data1 = json.load(data)
-            if data1['data']['assignee']['name']=='Yukta Anand':
-                task_name = data1['data']['name']
-                api = TodoistAPI('27113e1797906a73a11a7358805ac473499c414e')
-                api.sync()
-                project_name = data1['data']['memberships']['project']['name']
-                project1 = api.projects.add(project_name)
-                task1 = api.items.add(task_name, project1['id'])
-                api.commit()
+                    data1 = json.loads(data)
+                    if data1['data']['assignee']['name']=='Yukta Anand':
+                        task_name = data1['data']['name']
+                        api = TodoistAPI('27113e1797906a73a11a7358805ac473499c414e')
+                        api.sync()
+                        project_name = data1['data']['memberships']['project']['name']
+                        project1 = api.projects.add(project_name)
+                        api.items.add(task_name, project1['id'])
+                        api.commit()
         else:
             headers = request.headers
             x_hook = headers['X-Hook-Secret']
-            return JsonResponse({"X-Hook-Secret": data1})
-
+            return JsonResponse({"X-Hook-Secret": x_hook})
 
